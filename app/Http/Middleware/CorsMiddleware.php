7d665c92451a85b286477cd57c4469af37588711
Sirin k <sirin@signcatch.com>
2018-09-13 17:00:31 +0530

    Correction

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -16,7 +16,7 @@ class CorsMiddleware
     {
         $headers = [
             'Access-Control-Allow-Origin'      => '*',
-            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Methods'     => 'GET,POST,OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age'           => '86400',
            // 'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With',
