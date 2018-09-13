e72845842041fbf395ea900b736176b2e7f69c59
Sirin k <sirin@signcatch.com>
2018-09-13 17:07:30 +0530

    Correction

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -25,7 +25,7 @@ class CorsMiddleware
 
         if ($request->isMethod('OPTIONS'))
         {
-            return response()->json('{"method":"OPTIONS"}', 200, $headers);
+           // return response()->json('{"method":"OPTIONS"}', 200, $headers);
         }
 
         $response = $next($request);
